1bab7b4a3c9db265a6e86d20446f7a1ca43b1247
Mrd <m.rasool.dabaghian@gmail.com>
2025-04-12 16:37:08 +0330

    14040123 organ name

--- lib/config.dart
+++ lib/config.dart
@@ -4,11 +4,13 @@ class Config {
 
 
   static const String saveUserId = 'save_user_id';
-  static const String serverAddress ="http://localhost";
-  static const String serverAddressTraccar ="http://localhost:8082";
+  static const String serverAddress ="http://80.210.21.35";
+  static const String serverAddressTraccar ="http://80.210.21.35:8082";
   static const String serverTraccarPort ="8082";
 
   static const String mapAddress ="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
   //static const String mapAddress ="map1/tiles/{z}/{x}/{y}.png";
+
+  static const String organ ="آزادگان";
 
 }
