cabab0f8c4bea871c082620a54179ac0c865142f
Mrd <m.rasool.dabaghian@gmail.com>
2025-05-04 15:35:51 +0330

    14040214 change config struct

--- lib/config.dart
+++ lib/config.dart
@@ -4,14 +4,22 @@ class Config {
 
 
   static const String saveUserId = 'save_user_id';
+
+  /*
+  static const String organ ="آزادگان";
   static const String serverAddress ="http://80.210.21.35";
-  static const String serverAddressTraccar ="http://80.210.21.35:8082";
-  static const String serverTraccarPort ="8082";
+  static const String serverAddressTraccar ="http://80.210.21.35:36912";
+  */
+
+  static const String organ ="نیروی زمینی";
+  static const String serverAddress ="http://37.156.26.149:36915";
+  static const String serverAddressTraccar ="http://37.156.26.149:36912";
+
+
+  static const String serverTraccarPort ="36912";
 
   static const String mapAddress ="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
   //static const String mapAddress ="map1/tiles/{z}/{x}/{y}.png";
 
-
-  static const String organ ="آزادگان";
   static const String authEncryptionKey ="Dsr#^(963#^(9631";
 }
